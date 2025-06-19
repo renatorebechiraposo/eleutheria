de73a8952759fbb16eaf33f2554840b88aa07f9f
Renato <renatorebechiraposo@outlook.com>
2025-06-19 11:16:39 -0300

    feat: adicionar página de erro 404 personalizada

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Fredoka } from "next/font/google";
+import Image from "next/image";
+
+const fredoka = Fredoka({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-fredoka",
+});
+
+export default function NotFound() {
+  return (
+    <main
+      className={`${fredoka.className} flex flex-col items-center justify-center md:min-h-[calc(100vh-112px-148px)]`}
+    >
+      <p className="text-4xl md:text-6xl text-[#483417] font-bold text-center mt-9 md:mt-16 md:mb-4.5">
+        OPSS!!!
+      </p>
+      <Image
+        src={"/img/not-found.png"}
+        alt="Not Found"
+        width={300}
+        height={300}
+        className="md:mb-16"
+      />
+    </main>
+  );
+}
